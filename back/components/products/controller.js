cd9d8df7c5486ee3d523a6b1740891899aa36609
ttronge <tomastronge@gmail.com>
2022-10-16 21:36:20 -0300

    validations semi done

--- back/components/products/controller.js
+++ back/components/products/controller.js
@@ -1,8 +1,15 @@
 const store = require('./store');
 
-const addProduct = (product) => {
-    // TODO: add validation
-    return store.addProduct(product)
+const addProduct = product => {
+    return new Promise((resolve, reject) => {
+        if (Object.entries(product).length === 0) {
+            reject('Invalid data to create a product');
+            return false;
+        };
+        store.addProduct(product)
+            .then((result) => resolve(result))
+            .catch(e => reject('FALTA DATA'));
+    });
 };
 
 const getProducts = () => {
@@ -10,16 +17,38 @@ const getProducts = () => {
 };
 
 const getProductSingle = (id) => {
-    return Promise.resolve(store.getSingleProduct(id));
+    return new Promise(async (resolve, reject) => {
+        store.getSingleProduct(id)
+            .then(result => resolve(result))
+            .catch(err => reject('DONT EXIST ID'));
+    })
 };
 
-const editProduct = (id,newProduct) => {
-   return Promise.resolve(store.editProduct(id,newProduct))
-}
+const editProduct = (id, newProduct) => {
+    return new Promise(async (resolve, reject) => {
+        if (Object.entries(newProduct).length === 0 || !id) {
+            reject('invalid data, please send the changes for the product ');
+            return false;
+        }
+        store.editProduct(id, newProduct)
+            .then(result => resolve(result))
+            .catch(e => reject('Not exist id'))
+    });
+};
 
-const deleteProduct = id =>{
-    return Promise.resolve(store.deleteProduct(id))
-}
+const deleteProduct = id => {
+    return new Promise(async (resolve, reject) => {
+        if (!id) {
+            reject('invalid data, please send the changes for the product ');
+            return false;
+        }
+        store.deleteProduct(id)
+            .then(result => resolve(result))
+            .catch(e => reject('dont exit id'));
+    });
+};
+
+
 module.exports = {
     getProductSingle,
     addProduct,
--- back/components/products/network.js
+++ back/components/products/network.js
@@ -3,37 +3,39 @@ const router = express.Router();
 const { addProduct, getProducts, getProductSingle, editProduct, deleteProduct } = require('./controller');
 
 router.get('/:id', (req, res) => {
-    const { params: { id } } = req
+    const { params: { id } } = req;
     getProductSingle(id)
         .then((pr) => res.status(200).send(pr))
+        .catch(err => res.status(404).send(err));
 })
 
 router.get('/', (req, res) => {
     getProducts()
-        .then((pr) => res.status(200).send(pr))
+        .then((pr) => res.status(200).send(pr));
 })
 
 router.post('/', (req, res) => {
     const { body } = req;
     addProduct(body)
         .then(a => res.status(201).send({ status: 'created successfully.' }))
-        .catch(er => res.status(500).send({ status: 'There is an error , please try in another moment.' }))
-})
+        .catch(er => res.status(500).send({ status: er }));
+});
 
 router.put('/:id', (req, res) => {
-    const { params: { id }, body } = req
+    const { params: { id }, body } = req;
     editProduct(id, body)
         .then(a => res.status(201).send({ status: 'edited successfully.' }))
-        .catch(er => res.status(500).send({ status: 'There is an error , please try in another moment.' }))
-})
+        .catch(er => {
+            res.status(500).send({ status: er });
+        });
+});
 
 router.delete('/:id', (req, res) => {
-    const { params: { id } } = req
+    const { params: { id } } = req;
     deleteProduct(id)
         .then(a => res.status(201).send({ status: 'deleted successfully.' }))
-        .catch(er => res.status(500).send({ status: 'There is an error , please try in another moment.' }))
-
-})
+        .catch(er => res.status(500).send({ status: er }));
+});
 
 
 module.exports = router
