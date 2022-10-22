8dd19a98fb74d10253fb98add57ad260575d7677
ttronge <tomastronge@gmail.com>
2022-10-22 15:16:02 -0300

    with windows navigator user agent

--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import Header from './Components/Header';
 
+let test = window.navigator.userAgent.split(' ')
+let withparentesis = test[1].replace('(','')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
