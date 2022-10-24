a5a666c41c595113c7254abfd7e740759868e1df
agustinharmann <agustinharmann@gmail.com>
2022-10-24 16:47:59 -0300

    footer classname changes

--- front/src/Components/Footer/index.jsx
+++ front/src/Components/Footer/index.jsx
@@ -9,35 +9,22 @@ import './style.css';
 const Footer = () => (
 
   <footer className='container-footer'>
-
     <article className='content-footer'>
-
       <section className="content-footer-left">
-
         <div className="footer-left-first-column">
-
           <div className="content-img-footer">
-
-            <img className='img-logo-footer' src={LogoInThePear} alt="" />
-
+            <img className='img-logo-footer' src={LogoInThePear} alt="Logo footer" />
           </div>
-
           <p>Tienda de bebidas</p>
-
         </div>
-
-        
         <div className="footer-left-second-column">
-          <span className='content-txt-footer-left-second-column'> <a href="http://localhost:3000/">Cómo comprar</a> </span>
-          <span className='content-txt-footer-left-second-column'> <a href="http://localhost:3000/">Cancelar una compra</a> </span>
-          <span className='content-txt-footer-left-second-column'> <a href="http://localhost:3000/">Contactos</a> </span>
-          <span className='content-txt-footer-left-second-column'> <a href="http://localhost:3000/">Franquicias</a> </span>
+          <a className='txt-footer-left-second-column' href="http://localhost:3000/">Cómo comprar</a>
+          <a className='txt-footer-left-second-column' href="http://localhost:3000/">Cancelar una compra</a>
+          <a className='txt-footer-left-second-column' href="http://localhost:3000/">Contactos</a>
+          <a className='txt-footer-left-second-column' href="http://localhost:3000/">Franquicias</a>
         </div>
-
       </section>
-
       <section className="content-footer-rigth">
-
         <span className='content-footer-single-column-icons'>
           <a className='icons-footer-single-colum' href='none'>
             < BsFacebook size="25px" color='#FFFFFF' />
@@ -46,34 +33,23 @@ const Footer = () => (
             < BsInstagram size="25px" color='#FFFFFF' />
           </a>
         </span>
-        <span className='content-txt-footer-rigth-single-column'> <p>agustomi@gmail.com</p> </span>
-        <span className='content-txt-footer-rigth-single-column'> <p>1144444444</p> </span>
-        <span className='content-txt-footer-rigth-single-column'> <p>San Vicente 4444, San Martín, C4444</p> </span>
+        <p className='txt-footer-rigth-single-column'>agustomi@gmail.com</p>
+        <p className='txt-footer-rigth-single-column'>1144444444</p>
+        <p className='txt-footer-rigth-single-column'>San Vicente 4444, San Martín, C4444</p>
       </section>
-
     </article>
-
     <article className='content-post-footer'>
-
-
       <section className='post-footer-content-left'>
-
-        <span className="content-txt-post-footer-left"> <p>Powered By Agus y Tomi</p> </span>
-        <span className="content-txt-post-footer-left"> <p>Avisos Legales</p> </span>
-        <span className="content-txt-post-footer-left"> <p>¿Como cuidamos tu privacidad?</p> </span>
-
+        <p className="txt-post-footer-left">Powered By Agus y Tomi</p>
+        <p className="txt-post-footer-left">Avisos Legales</p>
+        <p className="txt-post-footer-left">¿Como cuidamos tu privacidad?</p>
       </section>
-
       <section className='post-footer-content-rigth'>
-
         <div className="content-img-post-footer">
-          <img className='img-logo-footer' src={LogoInThePearResponsive} alt="" />
+          <img className='img-logo-footer' src={LogoInThePearResponsive} alt="Logo post footer" />
         </div>
-
       </section>
-
     </article>
-
   </footer>
 )
 
