48137cf565efe0312af7613e3bd66abae99854ad
agustinharmann <agustinharmann@gmail.com>
2022-10-29 16:39:15 -0300

    changes class names and responsive details

--- front/src/Components/Footer/index.jsx
+++ front/src/Components/Footer/index.jsx
@@ -2,52 +2,51 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import LogoInThePear from '../../assets/LogoInThePear.png'
 import LogoInThePearResponsive from '../../assets/LogoInThePearResponsive.png';
-// CAMBIAR NOMBRE LOGO
 import { BsInstagram } from 'react-icons/bs';
 import { BsFacebook } from 'react-icons/bs';
 import './style.css';
 
 const Footer = () => (
 
-  <footer className='container-footer'>
-    <article className='content-footer'>
-      <section className="content-footer-left">
-        <div className="footer-left-first-column">
-          <div className="content-img-footer">
-            <img className='img-logo-footer' src={LogoInThePear} alt="Logo footer" />
+  <footer className='container__footer'>
+    <article className='content__footer'>
+      <section className="content__footer--left">
+        <div className="footer__left--first_column">
+          <div className="content__img--footer">
+            <img className='img__logo--footer' src={LogoInThePear} alt="Logo footer" />
           </div>
           <p>Tienda de bebidas</p>
         </div>
-        <div className="footer-left-second-column">
-          <Link className='txt-footer-left-second-column' to="#">Cómo comprar</Link>
-          <Link className='txt-footer-left-second-column' to="#">Cancelar una compra</Link>
-          <Link className='txt-footer-left-second-column' to="#">Contactos</Link>
-          <Link className='txt-footer-left-second-column' to="#">Franquicias</Link>
+        <div className="footer__left--second_column">
+          <Link className='footer__left--second_column-link' to="#">Cómo comprar</Link>
+          <Link className='footer__left--second_column-link' to="#">Cancelar una compra</Link>
+          <Link className='footer__left--second_column-link' to="#">Contactos</Link>
+          <Link className='footer__left--second_column-link' to="#">Franquicias</Link>
         </div>
       </section>
-      <section className="content-footer-rigth">
-        <span className='content-footer-single-column-icons'>
-          <Link className='icons-footer-single-colum' to='#'>
+      <section className="content__footer--rigth">
+        <span className='footer__rigth--single_column-icons'>
+          <Link className='icons__footer--single_colum' to='#'>
             < BsFacebook size="25px" color='#FFFFFF' />
           </Link>
-          <Link className='icons-footer-single-colum' to='https://www.instagram.com/agustindip_d/' >
+          <Link className='icons__footer--single_colum' to='https://www.instagram.com/agustindip_d/' >
             < BsInstagram size="25px" color='#FFFFFF' />
           </Link>
         </span>
-        <p className='txt-footer-rigth-single-column'>agustomi@gmail.com</p>
-        <p className='txt-footer-rigth-single-column'>1144444444</p>
-        <p className='txt-footer-rigth-single-column'>San Vicente 4444, San Martín, C4444</p>
+        <p className='footer__rigth--single_column'>agustomi@gmail.com</p>
+        <p className='footer__rigth--single_column'>1144444444</p>
+        <p className='footer__rigth--single_column'>San Vicente 4444, San Martín, C4444</p>
       </section>
     </article>
-    <article className='content-post-footer'>
-      <section className='post-footer-content-left'>
-        <p className="txt-post-footer-left">Powered By Agus y Tomi</p>
-        <p className="txt-post-footer-left">Avisos Legales</p>
-        <p className="txt-post-footer-left">¿Como cuidamos tu privacidad?</p>
+    <article className='content__post-footer'>
+      <section className='content__post-footer--left'>
+        <p className="post__footer--left">Powered By Agus y Tomi</p>
+        <p className="post__footer--left">Avisos Legales</p>
+        <p className="post__footer--left">¿Como cuidamos tu privacidad?</p>
       </section>
-      <section className='post-footer-content-rigth'>
-        <div className="content-img-post-footer">
-          <img className='img-logo-footer' src={LogoInThePearResponsive} alt="Logo post footer" />
+      <section className='content__post-footer--rigth'>
+        <div className="content__img--post-footer">
+          <img className='img__logo--footer' src={LogoInThePearResponsive} alt="Logo post footer" />
         </div>
       </section>
     </article>
