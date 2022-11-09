387afc8036a8ff73adacdd202ffac6edd97f251b
agustinharmann <agustinharmann@gmail.com>
2022-11-08 21:47:24 -0300

    finished carrusel component and add z-indez: 100; at header component

--- front/src/Components/Carrusel/index.jsx
+++ front/src/Components/Carrusel/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { useState } from "react";
+import { SlArrowLeft } from 'react-icons/sl';
+import { SlArrowRight } from 'react-icons/sl';
 import carrusel1 from '../../assets/carrusel/carrusel1.png';
 import carrusel2 from '../../assets/carrusel/carrusel2.png';
 import carrusel3 from '../../assets/carrusel/carrusel3.png';
@@ -28,76 +31,20 @@ const Carrusel = () => {
     condition ? setCarrusel(initialCarrusel) : setCarrusel(carrusel + 1);
   };
 
-
-  // const intervalo = setInterval(() => {
-  //   siguiente()
-  // }, 3000);
-  // let conta = 1;
-  // const cve = setInterval(()=>{
-  //   if (conta <= 6) {
-  //     console.log(`Han pasado ${conta} segundos`);
-  //   conta = conta + 1;
-  //   } else {
-  //     clearInterval(cve)
-  //   }
-    
-  // }, 1000)
-
-
   return (
-
-
-
     <article className="container__carrusel">
-      <a className="container__img--carrusel"
-      href="https://getbootstrap.com/"
+      <Link className="container__img--carrusel"
+        to="#"
       >
-        <img className="img__carrusel" src={imgCarrusel[carrusel]} alt={`imagen ${carrusel} del carrusel`} />
-      </a>
+        <img className="img__carrusel"
+          src={imgCarrusel[carrusel]}
+          alt={`imagen ${carrusel} del carrusel`} />
+      </Link>
       <section className="container__panel--carrusel">
-        <button className="btn__carrusel" onClick={ anterior }> ANTERIOR </button>
-        <button className="btn__carrusel" onClick={ siguiente }> SIGUIENTE </button>
+        <button className="btn__carrusel" onClick={anterior}> <SlArrowLeft size="40px" /> </button>
+        <button className="btn__carrusel" onClick={siguiente}> <SlArrowRight size="40px" /> </button>
       </section>
     </article>
-    // <>
-    //   <div className="container__carrusel">
-
-    //     <div className="container__img--carrusel">
-    //       <img className="img__carrusel"
-    //         src={imgCarrusel[carrusel]}
-    //         alt={`imagen ${carrusel} del carrusel`}
-    //       />
-    //     </div>
-
-
-
-    //     <div className="container__panel--carrusel">
-
-
-    //       <button
-    //         className="btn__carrusel"
-    //         onClick={anterior}
-    //       >
-    //         -1
-    //       </button>
-    //       <button
-    //         className="btn__carrusel"
-    //         onClick={siguiente}
-    //       >
-    //         +1
-    //       </button>
-
-
-    //     </div>
-
-
-
-
-
-
-    //   </div>
-    //   {/* <h1>counter: {carrusel}</h1> */}
-    // </>
   )
 };
 
