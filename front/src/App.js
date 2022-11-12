ade548bb69b677a0bc179980093f628941a7f950
agustinharmann <agustinharmann@gmail.com>
2022-11-12 00:24:48 -0300

    finished prefooter component

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,7 @@ import Filters from './Pages/Filters'
 import ErrorView from './Pages/ErrorView';
 import SingleProduct from './Pages/SingleProduct';
 import { UserProvider } from './contexts/UserContext';
+import  PreFooter  from './Components/PreFooter';
 import  Footer  from './Components/Footer';
 
 
@@ -18,6 +19,7 @@ function App() {
           <Route path='/producto/:name' element={<SingleProduct />} />
           <Route path='*' element={<ErrorView />} />
         </Routes>
+        <PreFooter />
         <Footer />
       </BrowserRouter>
     </UserProvider>
