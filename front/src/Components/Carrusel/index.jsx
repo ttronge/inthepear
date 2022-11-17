26e5cc318784fc23af3106df3f5f652997b0376a
agustinharmann <agustinharmann@gmail.com>
2022-11-17 14:35:41 -0300

    add ;, changes "" of className for , delete hover effect for button, add className at the icons, and changes name function of "siguiente" anda "anterior" at "next" and "prev", finished task

--- front/src/Components/Carrusel/index.jsx
+++ front/src/Components/Carrusel/index.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useState } from "react";
 import { SlArrowLeft } from 'react-icons/sl';
 import { SlArrowRight } from 'react-icons/sl';
 import carrusel1 from '../../assets/carrusel/carrusel1.png';
@@ -21,31 +20,29 @@ const Carrusel = () => {
   const initialCarrusel = 0;
   const [carrusel, setCarrusel] = useState(initialCarrusel);
 
-  const anterior = () => {
-    const condition = carrusel > 0;
-    condition ? setCarrusel(carrusel - 1) : setCarrusel(imgCarrusel.length - 1);
-  }
+  const back = () => {
+    carrusel > 0 ? setCarrusel(carrusel - 1) : setCarrusel(imgCarrusel.length - 1);
+  };
 
-  const siguiente = () => {
-    const condition = carrusel === 3;
-    condition ? setCarrusel(initialCarrusel) : setCarrusel(carrusel + 1);
+  const next = () => {
+    carrusel === 3 ? setCarrusel(initialCarrusel) : setCarrusel(carrusel + 1);
   };
 
   return (
-    <article className="container__carrusel">
-      <Link className="container__img--carrusel"
+    <article className='container__carrusel'>
+      <Link className='container__img--carrusel'
         to="#"
       >
-        <img className="img__carrusel"
+        <img className='img__carrusel'
           src={imgCarrusel[carrusel]}
           alt={`imagen ${carrusel} del carrusel`} />
       </Link>
-      <section className="container__panel--carrusel">
-        <button className="btn__carrusel" onClick={anterior}> <SlArrowLeft size="40px" /> </button>
-        <button className="btn__carrusel" onClick={siguiente}> <SlArrowRight size="40px" /> </button>
+      <section className='container__panel--carrusel'>
+        <button className='btn__carrusel' onClick={back}> <SlArrowLeft className='carrusel__icon' /> </button>
+        <button className='btn__carrusel' onClick={next}> <SlArrowRight className='carrusel__icon' /> </button>
       </section>
     </article>
-  )
+  );
 };
 
 export default Carrusel;
